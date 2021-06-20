662267e95be37ed832363df7204452c9e1e81505
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-19 23:22:08 -0700

    adding mutation type and resolver

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // 1. Require 'apollo-server'
 const { ApolloServer } = require("apollo-server");
 
-const typeDefs = ` type Query { totalPhotos: Int! } `;
+const typeDefs = ` 
+    type Query { totalPhotos: Int! } 
+    type Mutation { postPhoto(name: String! description: String): Boolean! }
+`;
+var photos = [];
+const resolvers = {
+  Query: { totalPhotos: () => photos.length },
 
-const resolvers = { Query: { totalPhotos: () => 42 } };
+  // Mutation and postPhoto resolver
+  Mutation: {
+    postPhoto(parent, args) {
+      photos.push(args);
+      return true;
+    },
+  },
+};
 
 // 2. Create a new instance of the server.
 // 3. Send it an object with typeDefs (the schema) and resolvers
