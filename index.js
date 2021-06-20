8ef091f73da8665db15ca9f6e0aa6d30c7d63abf
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-20 11:18:00 -0700

    resolver changes for the many to many connection

--- index.js
+++ index.js
@@ -94,11 +94,28 @@ const resolvers = {
     postedBy: (parent) => {
       return users.find((u) => u.githubLogin === parent.githubUser);
     },
+
+    taggedUsers: (parent) =>
+      tags
+        // Returns an array of tags that only contain the current photo
+        .filter((tag) => tag.photoID === parent.id)
+        // Converts the array of tags into an array of userIDs
+        .map((tag) => tag.userID)
+        // Converts array of userIDs into an array of user objects
+        .map((userID) => users.find((u) => u.githubLogin === userID)),
   },
   User: {
     postedPhotos: (parent) => {
       return photos.filter((p) => p.githubUser === parent.githubLogin);
     },
+    inPhotos: (parent) =>
+      tags
+        // Returns an array of tags that only contain the current user
+        .filter((tag) => tag.userID === parent.id)
+        // Converts the array of tags into an array of photoIDs
+        .map((tag) => tag.photoID)
+        // Converts array of photoIDs into an array of photo objects
+        .map((photoID) => photos.find((p) => p.id === photoID)),
   },
 };
 
