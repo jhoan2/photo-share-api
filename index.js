6380480f369aac7dab92d86c0fda1aabbecbe891
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-19 23:31:20 -0700

    type resolvers and a query for all photos with url

--- index.js
+++ index.js
@@ -2,19 +2,42 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = ` 
-    type Query { totalPhotos: Int! } 
-    type Mutation { postPhoto(name: String! description: String): Boolean! }
+    type Photo {
+        id: ID! 
+        url: String! 
+        name: String! 
+        description: String
+    }
+    type Query { 
+        totalPhotos: Int!
+        allPhotos: [Photo!]!
+    } 
+    type Mutation { 
+        postPhoto(name: String! description: String): Photo! 
+    }
 `;
+
+var _id = 0;
 var photos = [];
 const resolvers = {
-  Query: { totalPhotos: () => photos.length },
+  Query: {
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
 
   // Mutation and postPhoto resolver
   Mutation: {
     postPhoto(parent, args) {
-      photos.push(args);
-      return true;
+      var newPhoto = {
+        id: _id++,
+        ...args,
+      };
+      photos.push(newPhoto);
+      return newPhoto;
     },
+  },
+  Photo: {
+    url: (parent) => `http://yoursite.com/img/ ${parent.id} .jpg`,
   },
 };
 
