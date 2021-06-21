f15fbc1bd6f03d745fc19c1bc3392337315f59f2
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-20 21:15:25 -0700

    setup apollo-server-express and playground route

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // 1. Require 'apollo-server'
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
 const { GraphQLScalarType } = require("graphql");
+const expressPlayground =
+  require("graphql-playground-middleware-express").default;
+
+// 2. Call `express()` to create an Express application
+var app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// 3. Call `applyMiddleware()` to allow middleware mounted on the same path
+server.applyMiddleware({ app });
+
+// 4. Create a home route
+app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
+// play ground route
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+// 5. Listen on a specific port
+app.listen({ port: 4000 }, () =>
+  console.log(
+    `GraphQL Server running @ http://localhost:4000 ${server.graphqlPath} `
+  )
+);
 
 const typeDefs = ` 
     type User { 
