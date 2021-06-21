536f31665d36666d421be6ef75377632c5b1cf92
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-21 15:57:32 -0700

    added lib.js fx and resolver for githubAuth

--- resolvers/index.js
+++ resolvers/index.js
@@ -25,6 +25,39 @@ const resolvers = {
       photos.push(newPhoto);
       return newPhoto;
     },
+
+    async githubAuth(parent, { code }, { db }) {
+      //1. Obtain data from github
+      let { message, access_token, avatar_url, login, name } =
+        await authorizeWithGithub({
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code,
+        });
+      // 2. If there is a message, something went wrong
+      if (message) {
+        throw new Error(message);
+      }
+      // 3. Package the results into a single object
+      let latestUserInfo = {
+        name,
+        githubLogin: login,
+        githubToken: access_token,
+        avatar: avatar_url,
+      };
+      //4. Add or update the record with the new information
+      const {
+        ops: [user],
+      } = await db.collection("users").replaceOne(
+        {
+          githubLogin: login,
+        },
+        latestUserInfo,
+        { upsert: true }
+      );
+      //5. Return user data and their token
+      return { user, token: access_token };
+    },
   },
   Photo: {
     url: (parent) => `http://yoursite.com/img/ ${parent.id} .jpg`,
